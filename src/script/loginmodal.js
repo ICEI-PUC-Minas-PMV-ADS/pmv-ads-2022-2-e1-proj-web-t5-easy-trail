bedf4929a362a9592fd19656b3c3d9f97324dba1
Leonardo Lima <leodmelo@hotmail.com>
2022-10-30 13:35:38 -0300

    Updates
     - Troca imagem carrossel
     - Atualização na página de pesquisa de trilha
    - Atualizações no CSS

--- src/script/loginmodal.js
+++ src/script/loginmodal.js
@@ -1,11 +1,9 @@
-<script>
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+function login() {
+    var modal = document.getElementById('id01');
 }
-</script>
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            
+            modal.style.display = "none";
+        }
+}
