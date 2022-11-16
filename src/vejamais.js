9f9a0dc0209c07f3551d79083e1aa577f85df1f5
Leonardo Lima <leodmelo@hotmail.com>
2022-11-15 22:52:48 -0300

    Update
    - botão "Veja Mais" automático em todos os cards
    - Correção do texto do modal do botão "cadastre-se"
    - Criação página "Detalhe Trilha"
    - Inserção dos elementos do botão "Veja mais" no CSS e correção do JS

--- src/vejamais.js
+++ src/vejamais.js
@@ -1,16 +1,20 @@
-function vejaMais() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Veja Mais";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Veja Menos";
-      moreText.style.display = "inline";
+let noOfCharac = 150;
+let contents = document.querySelectorAll(".content");
+contents.forEach(content => {
+    //If text length is less that noOfCharac... then hide the read more button
+    if(content.textContent.length < noOfCharac){
+        content.nextElementSibling.style.display = "none";
     }
-  }
-  
+    else{
+        let displayText = content.textContent.slice(0,noOfCharac);
+        let moreText = content.textContent.slice(noOfCharac);
+        content.innerHTML = `${displayText}<span class="dots">...</span><span class="hide more">${moreText}</span>`;
+    }
+});
+
+function readMore(btn){
+    let post = btn.parentElement;
+    post.querySelector(".dots").classList.toggle("hide");
+    post.querySelector(".more").classList.toggle("hide");
+    btn.textContent == "Veja Mais" ? btn.textContent = "Veja Menos" : btn.textContent = "Veja Mais";
+}
