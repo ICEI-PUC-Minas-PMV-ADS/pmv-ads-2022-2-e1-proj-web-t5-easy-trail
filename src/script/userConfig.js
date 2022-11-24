2a2a4bfc148a4ce94c7b8773b964bc9e9770c91f
leonardobalves <leonardobuckalves@gmail.com>
2022-11-24 12:52:24 -0300

    alteracao pagina do usuario, funcionalidade de alterar nome, senha, email

--- src/script/userConfig.js
+++ src/script/userConfig.js
@@ -1,110 +1,8 @@
-// const dadosIniciais = {
-//     usuarios: [
-//         {
-//             login: "admin",
-//             senha: "123",
-//             nome: "Administrador do Sistema",
-//             email: "admin@abc.com",
-//             descricao: "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-//         },
-//         {
-//             login: "user",
-//             senha: "123",
-//             nome: "Usuario Comum",
-//             email: "user@abc.com",
-//             descricao: "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
-//         }
-//     ]
-// };
-
-// let readLog = JSON.parse(localStorage.getItem("usuariosCadastrados"));
-
-// if(localStorage.getItem("usuariosCadastrados") !== null){
-//     document.getElementById("user__name").innerHTML = readLog[1].nome;
-//     document.getElementById("user__password").innerHTML = readLog[1].senha;
-//     document.getElementById("user__email").innerHTML = readLog[1].email;
-//     document.getElementById("user__description").innerHTML = readLog[1].descricao;
-// } else {
-//     localStorage.setItem("usuariosCadastrados", JSON.stringify(dadosIniciais.usuarios));
-// }
-
-// function atualizaUsuarios() {
-//     localStorage.setItem("usuariosCadastrados", JSON.stringify(dadosIniciais.usuarios));
-//     readLog = JSON.parse(localStorage.getItem("usuariosCadastrados"));
-// }
-
-// function atualizaHTML() {
-//     document.getElementById("user__name").innerHTML = readLog[1].nome;
-//     document.getElementById("user__password").innerHTML = readLog[1].senha;
-//     document.getElementById("user__email").innerHTML = readLog[1].email;
-//     document.getElementById("user__description").innerHTML = readLog[1].descricao;
-// }
-
-// function alterarNome() {
-//     let inputUsuario = document.getElementById("input-nome").value;
-//     if(inputUsuario) {
-//         dadosIniciais.usuarios[1].nome = inputUsuario;
-//         atualizaUsuarios();
-//         atualizaHTML();
-//     }
-// }
-
-// function alterarSenha() {
-//     let inputSenha = document.getElementById("input-senha").value;
-//     if(inputSenha) {
-//         dadosIniciais.usuarios[1].senha = inputSenha;
-//         atualizaUsuarios();
-//         atualizaHTML();
-//     }
-// }
-
-// function alterarEmail() {
-//     let inputEmail = document.getElementById("input-email").value;
-//     if(inputEmail) {
-//         dadosIniciais.usuarios[1].email = inputEmail;
-//         atualizaUsuarios();
-//         atualizaHTML();
-//     }
-// }
-
-// function alterarDescricao() {
-//     let inputDescricao = document.getElementById("input-description").value;
-//     if(inputDescricao) {
-//         dadosIniciais.usuarios[1].descricao = inputDescricao;
-//         atualizaUsuarios();
-//         atualizaHTML();
-//     }
-// }
-
-// //BTN CONFIG
-
-// const btnShow = document.getElementById("user__configs");
-// const div2Show = document.getElementById("favoritas");
-// let clicked = false;
-
-// function showDiv(){
-//     if(clicked) {
-//         div2Show.style.display= "block";
-//         btnShow.style.display= "none";
-//         clicked = false;
-//     } else {
-//         div2Show.style.display= "none";
-//         btnShow.style.display= "block";
-//         clicked = true;
-//     }
-// }
-
 if(localStorage.getItem("trilhasFavoritas") === "[]" | localStorage.getItem("trilhasFavoritas") === null) {
     document.getElementById("nenhuma__favoritada").style.display = "block";
 } else {
     document.getElementById("nenhuma__favoritada").style.display = "none";
 }
-
-
-
-let meuModal = `
-    <div class="sharethis-inline-share-buttons"></div>
-`;
 
 let trilhasFavoritadas = JSON.parse(localStorage.getItem("trilhasFavoritas"));
 // console.log(trilhasFavoritadas);
@@ -125,16 +23,140 @@ trilhasFavoritadas.forEach((card) => {
     document.getElementById("cards__favoritas").innerHTML += cardFav;
 })
 
-let trilhasFavoritas  = [];
+let trilhasFavoritasS  = [];
 function removerFavoritos(trilha) {
-    trilhasFavoritas = JSON.parse(localStorage.getItem("trilhasFavoritas"));
-    trilhasFavoritas.splice(trilha.id, 1);
-    console.log(trilhasFavoritas.id);
-    localStorage.setItem("trilhasFavoritas", JSON.stringify(trilhasFavoritas));
+    trilhasFavoritasS = JSON.parse(localStorage.getItem("trilhasFavoritas"));
+    trilhasFavoritasS.splice(trilha.id, 1);
+    console.log(trilhasFavoritasS.id);
+    localStorage.setItem("trilhasFavoritas", JSON.stringify(trilhasSFavoritas));
     alert("Trilha removida!");
     window.location.reload();
 }
 
+const dadosIniciais = {
+    usuarios: [
+        {
+            login: "admin",
+            senha: "123",
+            nome: "Administrador do Sistema",
+            email: "admin@abc.com",
+            descricao: "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+        },
+        {
+            login: "user",
+            senha: "123",
+            nome: "Usuário Comum",
+            email: "user@abc.com",
+            descricao: "Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+        }
+    ]
+};
+
+
+for(i = 0; i < dadosIniciais.usuarios.length; i++){
+    dadosIniciais.usuarios.forEach((user) => {
+        localStorage.setItem(`user-${user.nome}`, JSON.stringify(user));
+    });
+}
+
+// let arraysLocalStg = []; // lista vazia para armazenar usuários
+// arraysLocalStg.filter(array => array.includes('user-'));
+
+// for (var i = 0; i < localStorage.length; i++) { // insere cada key do local storage na array
+//     arraysLocalStg.push(localStorage.key(i));
+// }
+// // console.log(arraysLocalStg);
+
+// let novosUsuarios = [];
+// var listaoUsuarios = arraysLocalStg.filter(array => array.includes('user-')); // separa todos os itens com texto "trilha-"
+// listaoUsuarios.forEach((item) => {
+//     var usuariosLogLog = JSON.parse(localStorage.getItem(item));
+//     // console.log(usuariosLogLog);
+//     novosUsuarios.push(usuariosLogLog);
+// });
+
+
+
+let cadastrados = JSON.parse(localStorage.getItem("usuariosCadastrados"));
+if(localStorage.getItem("usuariosCadastrados") !== null){
+    document.getElementById("NameUser").innerHTML = cadastrados[1].nome;
+    // document.getElementById("user__password").innerHTML = cadastrados[1].senha;
+    // document.getElementById("user__email").innerHTML = cadastrados[1].email;
+} else {
+    localStorage.setItem("usuariosCadastrados", JSON.stringify(dadosIniciais.usuarios));
+    window.reload();
+}
+
+function atualizaUsuarios() {
+    localStorage.setItem("usuariosCadastrados", JSON.stringify(dadosIniciais.usuarios));
+    cadastrados = JSON.parse(localStorage.getItem("usuariosCadastrados"));
+}
+
+function atualizaHTML() {
+    document.getElementById("NameUser").innerHTML = cadastrados[1].nome;
+//     document.getElementById("user__password").innerHTML = cadastrados[1].senha;
+//     document.getElementById("user__email").innerHTML = cadastrados[1].email;
+//     document.getElementById("user__description").innerHTML = cadastrados[1].descricao;
+    window.reload();
+}
+
+function alterarNome() {
+    let inputUsuario = document.getElementById("input-nome").value;
+    if(inputUsuario) {
+        dadosIniciais.usuarios[1].nome = inputUsuario;
+        atualizaUsuarios();
+        alert("Nome alterado com sucesso!")
+        atualizaHTML();
+    }
+}
+
+function alterarSenha() {
+    let inputSenha = document.getElementById("input-senha").value;
+    if(inputSenha) {
+        dadosIniciais.usuarios[1].senha = inputSenha;
+        atualizaUsuarios();
+        alert("Senha alterada com sucesso!")
+        atualizaHTML();
+    }
+}
+
+function alterarEmail() {
+    let inputEmail = document.getElementById("input-email").value;
+    if(inputEmail) {
+        dadosIniciais.usuarios[1].email = inputEmail;
+        atualizaUsuarios();
+        alert("E-mail alterado com sucesso!")
+        atualizaHTML();
+    }
+}
+
+// function alterarDescricao() {
+//     let inputDescricao = document.getElementById("input-description").value;
+//     if(inputDescricao) {
+//         dadosIniciais.usuarios[1].descricao = inputDescricao;
+//         atualizaUsuarios();
+//         atualizaHTML();
+//     }
+// }
+
+// //BTN CONFIG
+
+const userConfig = document.getElementById("user__configs");
+const trilhasFav = document.getElementById("trilhas__favoritas");
+let clickado = false;
+userConfig.style.display = "none";
+
+function showDivs(){
+    if(clickado) {
+        trilhasFav.style.display= "block";
+        userConfig.style.display= "none";
+        clickado = false;
+    } else {
+        trilhasFav.style.display= "none";
+        userConfig.style.display= "block";
+        clickado = true;
+    }
+}
 
 
 
